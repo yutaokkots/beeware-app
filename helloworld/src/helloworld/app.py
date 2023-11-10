40fbb1da8b76c903c3205865c683146174eba411
yutaokkots <yuta.okkotsu@gmail.com>
2023-11-09 20:36:39 -0700

    updating the app with changes to app.py

--- helloworld/src/helloworld/app.py
+++ helloworld/src/helloworld/app.py
@@ -41,7 +41,10 @@ class HelloWorld(toga.App):
         self.main_window.show()
 
     def say_hello(self, widget):
-        print(f"Hello, {self.name_input.value}")
+        self.main_window.info_dialog(
+            f"Hello, {self.name_input.value}",
+            "Hi!"
+        )
 
 def main():
     return HelloWorld()
